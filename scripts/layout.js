88ef1628784f844be8eb8b3555194072cad9e3fe
Ben Rosen <brosen@unca.edu>
2012-04-14 00:21:39 -0400

    post new event menu is much slicker. ids are all same so back-end compatability should be unchanged

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,14 +1,16 @@
-function slideUp(){
-	$(".screen").animate({"top": "+=100%"}, 300);
-
-	$("#menu_options").fadeOut("fast", function(){
-		$("#back").fadeIn("fast");
-	});
-}
-
 function toggleScreen(){
+
+	function slideUp(){
+		$(".screen").animate({"top": "+=100%"}, 300);
+
+		$("#menu_options").fadeOut("fast", function(){
+			$("#back").fadeIn("fast");
+		});
+	};
+
 	$("#post_event").click(function(){
 		$("#subscreen_post").show();
+		$("#input_gametype").focus();
 		slideUp();
 	});
 
@@ -27,16 +29,28 @@ function toggleScreen(){
 			$("#menu_options").fadeIn("fast");
 		});
 		$(".screen").animate({"top": "-=100%"}, 300, function(){
+			$(".position1").css("left", "50%")
+			$(".position2").css("left", "150%")
+			$(".position3").css("left", "250%")
+
 			$("#subscreen_post, #subscreen_find, #subscreen_menu").hide();
 		});
+	});
+};
+
+function navInputFields(){
+	$(".next_position").click(function(){
+		$(".input_field").animate({"left": "-=100%"});
+	});
+
+	$(".back_position").click(function(){
+		$(".input_field").animate({"left": "+=100%"});
 	});
 };
 
 $(document).ready(function(){
 
 	toggleScreen();
-	$(".event").click(function(){
-		$(this).css("background-color", "#fff")
-	});
+	navInputFields();
 
 });
